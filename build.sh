1a56ae8762dbfa154656e1e8004748d4dcb81a57
rashisaluja19 <rashisaluja0520@gmail.com.com>
2026-07-07 00:11:33 +0530

    Configure live database administration access accounts

--- build.sh
+++ build.sh
@@ -6,3 +6,20 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Force create or update the rashi superuser account on the live server
+python -c "
+import django
+django.setup()
+from django.contrib.auth.models import User
+username = 'rashi'
+email = ' '
+password = '12345@#$'
+user, created = User.objects.get_or_create(username=username)
+user.email = email
+user.set_password(password)
+user.is_superuser = True
+user.is_staff = True
+user.save()
+print('Live administrator account sync completed successfully!')
+"
