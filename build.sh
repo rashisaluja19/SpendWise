a880c927dfd546b871b02aca600c7c2429d9b9c7
rashisaluja19 <rashisaluja0520@gmail.com.com>
2026-07-07 00:24:15 +0530

    Fix python script path and force password update

--- build.sh
+++ build.sh
@@ -7,19 +7,20 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Force create or update the rashi superuser account on the live server
+# Force Django to know where your settings file is, then fix rashi's password
+export DJANGO_SETTINGS_MODULE="cute_expense_tracker.settings"
 python -c "
 import django
 django.setup()
 from django.contrib.auth.models import User
-username = 'rashi'
-email = ' '
-password = '12345@#$'
-user, created = User.objects.get_or_create(username=username)
-user.email = email
-user.set_password(password)
-user.is_superuser = True
-user.is_staff = True
-user.save()
-print('Live administrator account sync completed successfully!')
+try:
+    user = User.objects.get(username='rashi')
+    user.set_password('12345@#$')
+    user.is_superuser = True
+    user.is_staff = True
+    user.save()
+    print('Password successfully updated for existing rashi account!')
+except User.DoesNotExist:
+    User.objects.create_superuser('rashi', 'rashi@spendwise.local', '12345@#$')
+    print('Created brand new rashi superuser account!')
 "
